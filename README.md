223b89943ec8c9b5bf27e8a4fe7a1e119b3290cc
Hinka Patel <hinkapatel2994@gmail.com>
2021-06-18 00:16:01 -0700

    Some enhancements to models. Also,
     - Fixed required setup to run it with mysql using migrations.
     - Updated requirements.txt
     - Moved project_setup instructions to README.md

---
+++ README.md
@@ -0,0 +1,33 @@
+## TaskManagementSystem
+Task Management System allows for easy tracking of task and responsibilities across organization.
+
+## Develpement Guide
+### Setting up Database
+The backend service is a flask based service. Follow the steps to set the environment to run the flask app.
+Step 1: Clone the repo and move to staff dir. Following steps assume that your current working dir is `<YourProjectPath>/Tms/staff/`
+```
+git clone <>
+cd Tms/staff/
+```
+Step 2: Install mySQL server and create username and password.
+Step 4: Set mysql url in environment
+```
+export DATABASE_URL=mysql://<USERNAME>:<PASSWORD>@localhost/<DATABASE_NAME>
+```
+Step 3: Install python dependencies.
+```
+pip3 install -r requirements.txt --user
+```
+Step 4: Run Migrations
+```
+export FLASK_APP=models.py
+flask db init <--- RUN ONLY IF migrations/ folder is missing. If running it first time.
+flask db migrate
+flask db upgrade head
+```
+Notes
+- If flask command line is missing, make sure that your $PATH env variable has path where flask was installed.
+
+### Setting up Webservice
+To run the URLs please run "views.py" file.
+Open this URL "http://127.0.0.1:5000/" to see "Hello World!"
--- staff/models.py
+++ staff/models.py
@@ -1,11 +1,15 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from sqlalchemy_utils import PasswordType
+import enum
 
 app = Flask(__name__)
 # Below is the set up for database where database=tms, db_password=password12, db_user=root, db_host=localhost
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:password12@localhost/tms'
+if not os.getenv("DATABASE_URL", None):
+    raise Exception("DATABASE_URL not set in environment (mysql://username:password@localhost/tms")
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL", None)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
@@ -13,108 +17,160 @@ migrate = Migrate(app, db)
 
 class Base(db.Model):
     __abstract__ = True
-    created_date = db.Column(db.DateTime, index=False, unique=False, nullable=False)
-    modified_date = db.Column(db.DateTime, index=False, unique=False, nullable=False)
+    id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
+    created_date = db.Column(db.DateTime, nullable=False, server_default=db.func.now())
+    modified_date = db.Column(db.DateTime, nullable=False, server_default=db.func.now())
 
 
-class Department(db.Model):
+class Department(Base):
     """ Model for Departments list """
-
     __tablename__ = 'department'
-    id = db.Column(db.Integer, primary_key=True)
     department_name = db.Column(db.String(80), unique=True, nullable=False)
-    department_email = db.Column(db.String(120), unique=True, nullable=False)
+    department_email = db.Column(db.String(200), unique=True, nullable=False)
 
     def __repr__(self):
         return '<Department Name  %r>' % self.department_name
 
 
-class TaskType(Base):
-    """ Model for Task Types list """
+class TaskType(enum.Enum):
+    """ Model for Task Types """
 
     __tablename__ = 'task_type'
-    id = db.Column(db.Integer, primary_key=True)
-    task_type = db.Column(db.String(80), unique=True, nullable=False)
+    QUESTION = "QUESTION"
+    INCIDENT = "INCIDENT"
+    PROBLEM = "PROBLEM"
+    FEATURE_REQUEST = "FEATURE_REQUEST"
+    REFUND = "REFUND"
 
-    def __repr__(self):
-        return '<Task-Type  %r>' % self.task_type
+    @staticmethod
+    def from_task_type(string_val):
+        mapping = {
+            "QUESTION": TaskType.QUESTION,
+            "INCIDENT": TaskType.INCIDENT,
+            "PROBLEM": TaskType.PROBLEM,
+            "FEATURE_REQUEST": TaskType.FEATURE_REQUEST,
+            "REFUND": TaskType.REFUND
+        }
+
+        val = mapping.get(string_val)
+        if not val:
+            raise Exception("Task Type {} is not implemented".format(string_val))
+        return val
 
 
-class TaskPriority(Base):
-    """ Model for Task Priority list """
+class TaskPriority(enum.Enum):
+    """ Model for Task Priority """
 
     __tablename__ = 'task_priority'
-    id = db.Column(db.Integer, primary_key=True)
-    task_priority = db.Column(db.String(80), unique=True, nullable=False)
+    LOW = "LOW"
+    MEDIUM = "MEDIUM"
+    HIGH = "HIGH"
+    URGENT = "URGENT"
 
-    def __repr__(self):
-        return '<Task-Priority-  %r>' % self.task_priority
+    @staticmethod
+    def from_task_priority(string_val):
+        mapping = {
+            "LOW": TaskPriority.LOW,
+            "MEDIUM": TaskPriority.MEDIUM,
+            "HIGH": TaskPriority.HIGH,
+            "URGENT": TaskPriority.URGENT
+        }
+
+        val = mapping.get(string_val)
+        if not val:
+            raise Exception("Priority {} is not implemented".format(string_val))
+        return val
 
 
-class TaskState(Base):
-    """ Model for Task State list """
+class TaskState(enum.Enum):
+    """ Model for Task State """
 
     __tablename__ = 'task_state'
-    id = db.Column(db.Integer, primary_key=True)
-    task_state = db.Column(db.String(80), unique=True, nullable=False)
+    SCREEN = "SCREEN"
+    ASSIGNED = "ASSIGNED"
+    RESOLVED = "RESOLVED"
+    BLOCKED = "BLOCKED"
+    UNBLOCKED = "UNBLOCKED"
 
-    def __repr__(self):
-        return '<Task-state  %r>' % self.task_state
+    @staticmethod
+    def from_task_state(string_val):
+        mapping = {
+            "SCREEN": TaskState.SCREEN,
+            "ASSIGNED": TaskState.ASSIGNED,
+            "RESOLVED": TaskState.RESOLVED,
+            "BLOCKED": TaskState.BLOCKED,
+            "UNBLOCKED": TaskState.UNBLOCKED
+        }
 
+        val = mapping.get(string_val)
+        if not val:
+            raise Exception("Task State {} is not implemented".format(string_val))
 
-class Role(Base):
-    """ Model for staff Role list """
+class Role(enum.Enum):
+    """ Model for staff Role """
 
     __tablename__ = 'role'
-    id = db.Column(db.Integer, primary_key=True)
-    role_type = db.Column(db.String(80), unique=True, nullable=False)
+    ADMIN = "ADMIN"
+    REGULAR = "REGULAR"
 
-    def __repr__(self):
-        return '<Role  %r>' % self.role_type
+    @staticmethod
+    def from_str(string_val):
+        mapping = {
+            "ADMIN": Role.ADMIN, # full access.
+            "REGULAR": Role.REGULAR # only read write access.
+        }
+
+        val = mapping.get(string_val)
+        if not val:
+            raise Exception("Role {} is not implemented".format(string_val))
+
+        return val
+
+
+class UserType(enum.Enum):
+    """ Model for staff Role """
+
+    __tablename__ = 'role'
+    STUDENT = "STUDENT"
+    EMPLOYEE = "EMPLOYEE"
+
+    @staticmethod
+    def from_str(string_val):
+        mapping = {
+            "STUDENT": Role.STUDENT,
+            "EMPLOYEE": Role.EMPLOYEE
+        }
+
+        val = mapping.get(string_val)
+        if not val:
+            raise Exception("User Type {} is not implemented".format(string_val))
+
+        return val
 
 
 class User(Base):
     """ Model for User details """
-
-    __tablename__ = 'user'
-    id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(50), unique=False, nullable=False)
-    last_name = db.Column(db.String(50), unique=False, nullable=True)
-    email = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(PasswordType(
-        schemes=[
-            'pbkdf2_sha512',
-            'md5_crypt'
-        ],
-
-        deprecated=['md5_crypt']
-    ), nullable=False)
-
-    role_id = db.Column(db.Integer, db.ForeignKey('role.id'), unique=False, nullable=False)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=True)
+    email = db.Column(db.String(200), unique=True, nullable=False)
+    password = db.Column(db.Text, nullable=False)
+    role = db.Column(db.Enum(Role))
+    user_type = db.Column(db.Enum(UserType))
+    itu_id = db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
         return '<User(Firstname=%s, Email=%s)>' % (self.first_name, self.email)
 
 
-class Employee(Base):
-    """ Model for Employees list """
 
-    __tablename__ = 'employee'
-    id = db.Column(db.Integer, primary_key=True)
-    itu_emp_id = db.Column(db.Integer, unique=True, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), unique=True, nullable=False)
-
-    def __repr__(self):
-        return '<Itu-EMP-ID  %d>' % self.itu_emp_id
-
-
-class EmployeeDepartment(Base):
+class EmployeeDepartmentMapping(Base):
     """ Model for Employee's Department """
 
-    __tablename__ = 'employee_department'
-    id = db.Column(db.Integer, primary_key=True)
-    dept_id = db.Column(db.Integer, db.ForeignKey('department.id'), unique=True, nullable=False)
-    emp_id = db.Column(db.Integer, db.ForeignKey('employee.id'), unique=True, nullable=False)
+    __tablename__ = 'employee_department_mapping'
+
+    dept_id = db.Column(db.Integer, db.ForeignKey('department.id'), nullable=False)
+    emp_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    db.UniqueConstraint(dept_id, emp_id)
 
     def __repr__(self):
         return '<EmpDpt(DeptId=%d, EmpId=%d)>' % (self.dept_id, self.emp_id)
@@ -124,14 +180,15 @@ class Task(Base):
     """ Model for Task """
 
     __tablename__ = 'task'
-    id = db.Column(db.Integer, primary_key=True)
-    task_title = db.Column(db.String(50), unique=False, nullable=False)
-    description = db.Column(db.String(100), unique=False, nullable=True)
-    task_state_id = db.Column(db.Integer, db.ForeignKey('task_state.id'), unique=True, nullable=False)
-    task_priority_id = db.Column(db.Integer, db.ForeignKey('task_priority.id'), unique=True, nullable=False)
-    department_id = db.Column(db.Integer, db.ForeignKey('department.id'), unique=True, nullable=False)
-    assignee_id = db.Column(db.Integer, db.ForeignKey('employee.id'), unique=True, nullable=False)
-    originator_id = db.Column(db.Integer, db.ForeignKey('user.id'), unique=True, nullable=False)
+
+    task_title = db.Column(db.String(200), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    task_type = db.Column(db.Enum(TaskType))
+    task_state = db.Column(db.Enum(TaskState))
+    task_priority = db.Column(db.Enum(TaskPriority))
+    department_id = db.Column(db.Integer, db.ForeignKey('department.id'), nullable=False)
+    assignee_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    originator_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
         return '<(TaskTitle=%d)>' % (self.task_title)
@@ -141,20 +198,32 @@ class Discussion(Base):
     """ Model for Discussion  """
 
     __tablename__ = 'discussion'
-    id = db.Column(db.Integer, primary_key=True)
-    discussion_text = db.Column(db.String(100), unique=False, nullable=True)
-    originator_id = db.Column(db.Integer, db.ForeignKey('user.id'), unique=False, nullable=True)
-    task_id = db.Column(db.Integer, db.ForeignKey('task.id'), unique=True, nullable=False)
+
+    discussion_text = db.Column(db.Text, nullable=True)
+    originator_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
+    task_id = db.Column(db.Integer, db.ForeignKey('task.id', ondelete="CASCADE"), nullable=False)
 
 
 class Attachment(Base):
     """ Model for Attachment  """
 
     __tablename__ = 'attachment'
-    id = db.Column(db.Integer, primary_key=True)
-    attachment_text = db.Column(db.String(100), unique=False, nullable=True)
-    attachment_url = db.Column(db.Integer, unique=True, nullable=True)
-    originator_id = db.Column(db.Integer, db.ForeignKey('user.id'), unique=False, nullable=True)
+
+    attachment_name = db.Column(db.Text, nullable=True)
+    attachment_url = db.Column(db.Text, nullable=True)
+    originator_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
+    task_id = db.Column(db.Integer, db.ForeignKey('task.id', ondelete="CASCADE"), nullable=False)
 
     def __repr__(self):
         return '<(Attachment URL=%r)>' % (self.attachment_url)
+
+
+class ScreenerInfo(Base):
+    """Stores info for department vs screener"""
+    __tablename__ = 'screener_info'
+
+    dept_id = db.Column(db.Integer, db.ForeignKey('department.id'), unique=True, nullable=False)
+    emp_id = db.Column(db.Integer, db.ForeignKey('user.id'), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<ScreenerInfo(DeptId=%d, EmpId=%d)>' % (self.dept_id, self.emp_id)

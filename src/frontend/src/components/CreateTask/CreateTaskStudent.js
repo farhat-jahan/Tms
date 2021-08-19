7c44f3f0df0890842b3e3634966992fbfb87eb3b
Hinka Patel <hinkapatel2994@gmail.com>
2021-08-19 14:37:09 -0700

    Create Inquiry integration

--- src/frontend/src/components/CreateTask/CreateTaskStudent.js
+++ src/frontend/src/components/CreateTask/CreateTaskStudent.js
@@ -1,9 +1,14 @@
-import React, {useRef, useState} from "react";
+import React, {useRef, useState, useEffect} from "react";
 import './CreateTaskBase.css';
-import {Button, Modal} from "react-bootstrap";
+import axios from 'axios';
+import { Button, Modal } from "react-bootstrap";
+import { useHistory } from 'react-router-dom';
+import { getToken } from '../Utils/Common';
 import ituCongrats from './Congrats.svg';
 
 const InquiryForm = () => {
+    const history = useHistory();
+    const studentDashboard = () => history.push('/student');
 
     const [draftState, setDraftState] = useState(false);
 
@@ -14,11 +19,57 @@ const InquiryForm = () => {
     const onFileInputChange = (event) => {
         const { files } = event.target;
         console.log(files);
-        // do something with your files...
     }
 
     const onTargetClick = () => {
         fileInputRef.current.click()
+    }
+
+    const inquiryTitle = useFormInput('');
+    const departmentSelection = useFormInput('');
+    const inquiryDescription = useFormInput('');
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [options, setOptions] = useState(null);
+
+    let config = {
+        headers : {
+            'Authorization': 'Bearer ' + getToken()
+        }
+    }
+
+    useEffect(() => {
+        axios.get('http://localhost:5000/api/v1/department-list', config).then(response=>{
+        setOptions(response.data);      
+    })
+    }, []);
+
+    const createInquiry = () => {
+        let config = {
+            headers : {
+                'Authorization': 'Bearer ' + getToken()
+            }
+        }
+
+        const reqBody = {
+                            title: inquiryTitle.value,
+                            department: "ADMISSIONS",
+                            description :inquiryDescription.value,
+                            taskType: "QUESTION",
+                            task_state: "ASSIGNED",
+                            task_priority: "MEDIUM",
+                            studentId: 62312
+                        }
+
+        axios.post('http://localhost:5000/api/v1/create-task-student', reqBody, config).then(response => {
+            setLoading(false);
+            setSubmitState(!submitState);
+        }).catch(error => {
+            setLoading(false);
+            if (error.response && error.response.status === 401) setError(error.response.data.message);
+            else if (error.response && error.response.status === 500) setError(error.message);
+            else setError("Something went wrong. Please try again later.");
+        });
     }
 
     return (
@@ -27,20 +78,27 @@ const InquiryForm = () => {
             <fieldset>
                 <div className="form-group">
                     <label htmlFor="inquiry-title" className="form-label mt-4 Tms-input-label">TITLE</label>
-                    <input type="text" className="form-control Tms-input-field Tms-imput-field-text" id="inquiry-title" placeholder="Type something"/>
+                    <input 
+                        {...inquiryTitle}
+                        type="text" 
+                        className="form-control Tms-input-field Tms-imput-field-text" 
+                        id="inquiryTitle" 
+                        placeholder="Type something"
+                    />
                 </div>
 
                 <div className="form-group">
                     <div className="row">
                         <div className="col-md-6">
                             <label htmlFor="inquiry-title" className="form-label mt-4 Tms-input-label">DEPARTMENT</label>
-                            <select className="form-control Tms-input-field" id="departmentselection">
-                                <option>Select</option>
+                            <select {...departmentSelection} className="form-control Tms-input-field" id="departmentSelection">
+                                {/* <option>Select</option>
                                 <option>Admissions</option>
                                 <option>ISO</option>
                                 <option>Registrar</option>
                                 <option>Advising</option>
-                                <option>Accounting</option>
+                                <option>Accounting</option> */}
+                                {options?.map(({ id, department_name }, index) => <option value={id} >{department_name}</option>)}
                             </select>
                         </div>
                         <div className="col-md-6">
@@ -59,7 +117,13 @@ const InquiryForm = () => {
 
                 <div className="form-group">
                     <label htmlFor="inquiry-description" className="form-label mt-4 Tms-input-label">DESCRIPTION</label>
-                    <textarea className="form-control Tms-input-field Tms-imput-field-text" id="inquiry-description" placeholder="Type something" rows="10"/>
+                    <textarea 
+                        {...inquiryDescription}
+                        className="form-control Tms-input-field Tms-imput-field-text" 
+                        id="inquiryDescription" 
+                        placeholder="Type something" 
+                        rows="10"
+                    />
                 </div>
                 
                 <div className="form-group">
@@ -93,7 +157,7 @@ const InquiryForm = () => {
                 </div>
                 <br/><br/><br/><br/>
                 <div className="form-group">
-                    <Button className="btn btn-md Tms-btn-primary inquiry-submit-btn trigger" onClick={() => setSubmitState(!submitState)}> Submit </Button>
+                    <Button className="btn btn-md Tms-btn-primary inquiry-submit-btn trigger" onClick={() => createInquiry()}> Submit </Button>
                     <Modal dialogClassName="my-modal" show={submitState}>
                         <Modal.Body className="justify-content-center">
                             <br/><br/>
@@ -103,8 +167,8 @@ const InquiryForm = () => {
                             <p className="Tms-para3 text-center">You have successfully submitted an inquiry.</p>
                         </Modal.Body>
                         <Modal.Footer className="custom-modal-footer">
-                            <div class="align-self-center mx-auto">
-                                <Button className="btn btn-md Tms-btn-primary" onClick={() => setSubmitState(false)}>Go to Dashboard</Button>
+                            <div className="align-self-center mx-auto">
+                                <Button className="btn btn-md Tms-btn-primary" onClick={() => studentDashboard()} >Go to Dashboard</Button>
                             </div>
                         </Modal.Footer>
                         <br/><br/>
@@ -131,13 +195,15 @@ const InquiryForm = () => {
 
 
 function CreateTaskStudent() {
+    const history = useHistory();
+    const studentDashboard = () => history.push('/student');
 
     return (
         <div className="container-fluid Tms-page-bg">
             <br/><br/><br/>
             <div className="row">
                 <div className="col col-md-6 offset-md-3">
-                    <a href="#" className="card-link text-decoration-none Tms-para4"> &lt; Back to Listings</a>
+                    <a href="#" className="card-link text-decoration-none Tms-para4" onClick={studentDashboard}> &lt; Back to Listings</a>
                     <div className="card inquiry-form">
                         <div className="card-body">
                             <h4 className="card-title Tms-h4">Submit an Inquiry</h4>
@@ -150,6 +216,18 @@ function CreateTaskStudent() {
             
         </div>
     );
+}
+
+const useFormInput = initialValue => {
+    const [value, setValue] = useState(initialValue);
+   
+    const handleChange = e => {
+      setValue(e.target.value);
+    }
+    return {
+      value,
+      onChange: handleChange
+    }
 }
 
 export default CreateTaskStudent;
